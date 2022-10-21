3745ae0331fcedd5b3cfa92bc80d6e5e65242a82
balajis <balajis@ADFL-1210.ADFDATA.NET>
2022-10-21 20:21:09 +0530

    Initial commit

---
+++ src/main/java/com/example/JarsToNexusDependency/JarsToNexusDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.JarsToNexusDependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarsToNexusDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarsToNexusDependencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JarsToNexusDependency/JarsToNexusDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JarsToNexusDependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarsToNexusDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
